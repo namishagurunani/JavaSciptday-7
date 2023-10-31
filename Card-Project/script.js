22fe6eab733af686ac6f0a6129daa04cb6b15922
shivam gurunani <namisha.gurunani173261@gmail.com>
2023-10-31 11:51:11 +0530

    Project Completed

--- Card-Project/script.js
+++ Card-Project/script.js
@@ -1,55 +1,70 @@
 const userForm = document.getElementById('userForm');
-        const userCard = document.getElementById('userCard');
-        const themeSelect = document.getElementById('themeSelect');
+const userCard = document.getElementById('userCard');
+const themeSelect = document.getElementById('themeSelect');
 
-        // Function to update the displayed user information
-        function updateUserCard() {
-            const name = document.getElementById('name').value;
-            const phone = document.getElementById('phone').value;
-            const village = document.getElementById('village').value;
-            const city = document.getElementById('city').value;
+// Function to update the displayed user information
+function updateUserCard() {
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const country = document.getElementById('country').value;
+    const state = document.getElementById('state').value;
+    const phone = document.getElementById('phone').value;
+    const village = document.getElementById('village').value;
+    const city = document.getElementById('city').value;
 
-            document.getElementById('displayedName').textContent = name;
-            document.getElementById('displayedPhone').textContent = phone;
-            document.getElementById('displayedVillage').textContent = village;
-            document.getElementById('displayedCity').textContent = city;
+    document.getElementById('displayedFirstName').textContent = firstName;
+    document.getElementById('displayedLastName').textContent = lastName;
+    document.getElementById('displayedCountry').textContent = country;
+    document.getElementById('displayedState').textContent = state;
+    document.getElementById('displayedPhone').textContent = phone;
+    document.getElementById('displayedVillage').textContent = village;
+    document.getElementById('displayedCity').textContent = city;
 
-            // Store data in local storage
-            localStorage.setItem('name', name);
-            localStorage.setItem('phone', phone);
-            localStorage.setItem('village', village);
-            localStorage.setItem('city', city);
-        }
+    // Store data in local storage
+    localStorage.setItem('firstName', firstName);
+    localStorage.setItem('lastName', lastName);
+    localStorage.setItem('country', country);
+    localStorage.setItem('state', state);
+    localStorage.setItem('phone', phone);
+    localStorage.setItem('village', village);
+    localStorage.setItem('city', city);
+}
 
-        // Function to load user information from local storage
-        function loadUserFromLocalStorage() {
-            const name = localStorage.getItem('name');
-            const phone = localStorage.getItem('phone');
-            const village = localStorage.getItem('village');
-            const city = localStorage.getItem('city');
+// Function to load user information from local storage
+function loadUserFromLocalStorage() {
+    const firstName = localStorage.getItem('firstName');
+    const lastName = localStorage.getItem('lastName');
+    const country = localStorage.getItem('country');
+    const state = localStorage.getItem('state');
+    const phone = localStorage.getItem('phone');
+    const village = localStorage.getItem('village');
+    const city = localStorage.getItem('city');
 
-            if (name) {
-                document.getElementById('name').value = name;
-                document.getElementById('phone').value = phone;
-                document.getElementById('village').value = village;
-                document.getElementById('city').value = city;
+    if (firstName) {
+        document.getElementById('firstName').value = firstName;
+        document.getElementById('lastName').value = lastName;
+        document.getElementById('country').value = country;
+        document.getElementById('state').value = state;
+        document.getElementById('phone').value = phone;
+        document.getElementById('village').value = village;
+        document.getElementById('city').value = city;
 
-                updateUserCard();
-            }
-        }
+        updateUserCard();
+    }
+}
 
-        // Event listener for form submission
-        userForm.addEventListener('submit', function (e) {
-            e.preventDefault();
-            updateUserCard();
-        });
+// Event listener for form submission
+userForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    updateUserCard();
+});
 
-        // Event listener for theme selection
-        themeSelect.addEventListener('change', function () {
-            const selectedTheme = themeSelect.value;
-            document.body.className = selectedTheme;
-            userCard.className = `card ${selectedTheme}`;
-        });
+// Event listener for theme selection
+themeSelect.addEventListener('change', function () {
+    const selectedTheme = themeSelect.value;
+    document.body.className = selectedTheme;
+    userCard.className = `card ${selectedTheme}`;
+});
 
-        // Load user information from local storage
-        loadUserFromLocalStorage();
+// Load user information from local storage
+loadUserFromLocalStorage();
--- Local-Storage-Project/script.js
+++ Local-Storage-Project/script.js
@@ -1,4 +1,5 @@
 const textarea = document.getElementById("myTextarea");
+const themeSelect = document.getElementById('themeSelect');
 
 // Function to save the textarea content to local storage
 function saveToLocalStorage() {
@@ -12,3 +13,12 @@ if (localStorage.getItem("savedText")) {
 
 // Add an event listener to the textarea for input changes and save to local storage
 textarea.addEventListener("input", saveToLocalStorage);
+// Event listener for theme selection
+themeSelect.addEventListener('change', function () {
+    const selectedTheme = themeSelect.value;
+    document.body.className = selectedTheme;
+    userCard.className = `card ${selectedTheme}`;
+});
+
+// Load user information from local storage
+loadUserFromLocalStorage();
